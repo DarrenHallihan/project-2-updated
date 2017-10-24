c2e2ef71720047bfd96c5dad4138b3c5ecf716da
DarrenHallihan <32300898+DarrenHallihan@users.noreply.github.com>
2017-10-23 22:27:56 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -27,40 +27,73 @@ public class Main {
             try {
                 userInput = scanner.nextInt();
                 if (userInput < 1 || userInput > 4)
-                    throw new Exception("Error, bad input, ");
+                    throw new Exception("Error, bad input, please enter a number 1-4");
             }
             catch (Exception userError){
-                System.out.println(userError + "please enter a number 1-4");
+                System.out.println("Error, bad input, please enter a number 1-4");
                 System.out.println("");
                 System.out.println("");
             }
             if(userInput == 1){
                 alphabeticalSort(characters);
+                System.out.println("");
+                System.out.println("");
             }
             if(userInput == 2){
                 frequencySort(characters);
+                System.out.println("");
+                System.out.println("");
 
             }
             if(userInput == 3){
                 charTypes(characters);
+                System.out.println("");
+                System.out.println("");
             }
 
 
 
 
             }
+        System.out.println("");
+        System.out.println("");
         System.out.println("Character Sorter Successfully Exited");
 
         }
     public static void alphabeticalSort(int[] charArray){
         for(int i = 0; i < 256; i++){
             if(charArray [i] != 0){
-                System.out.println((char)(i) + "freq:" + charArray[i]);
+                System.out.println((char)(i) + " freq: " + charArray[i]);
             }
         }
 
     }
     public static void frequencySort(int[] userArray){
+        int temp = 0;
+        char tempChar = 0;
+        int freqArray[] = new int[256];
+        char charArray[] = new char[256];
+        for(int i = 0; i < 256; i++){
+            freqArray[i] = 0;
+            charArray[i] = '0';
+        }
+        for(int i = 0; i < 256; i++){
+            for(int j = 0; j < 256; j++){
+                if(charArray[j] > charArray[j + 1]){
+                    tempChar = charArray[j];
+                    charArray[j] = charArray[j + 1];
+                    charArray[j+1] = tempChar;
+                if(freqArray[j] > freqArray[j + 1]){
+                    temp = freqArray[j];
+                    freqArray[j] = freqArray[j+1];
+                    freqArray[j+1] = temp;
+                }
+
+
+                }
+            }
+        }
+
 
     }
     public static void charTypes(int[] charArray){
